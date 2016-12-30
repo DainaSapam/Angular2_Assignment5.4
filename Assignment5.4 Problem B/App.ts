a3e29227bff8988f3eb3345f8682b06790938ff9
DainaSapam <dainasapam8@gmail.com>
2016-12-30 15:36:02 +0530

    Add files via upload

---
+++ Assignment5.4 Problem B/App.ts
@@ -0,0 +1,17 @@
+import * from "./Math";
+class BasicMath{
+    add(x: 5, y: 3) {
+        console.log(x + y);
+    }
+
+    diff(x: 5, y: 2) {
+        console.log(x - y);
+    }
+
+    mul(x: 6, y: 3) {
+        console.log(x * y);
+    }
+    div(x: 8, y: 2) {
+        console.log(x / y);
+    }
+}
